fe4fb01b134c335943a8f2ae1f6d5d6bb9e868f7
lmpizarro <lmpbici@gmail.com>
2016-06-09 21:16:52 -0300

    util/filters init cont.

--- util/filters.h
+++ util/filters.h
@@ -14,6 +14,10 @@ typedef struct {
 
 }low_pass_filter;
 
+low_pass_filter *low_pass_filter_new(const float fc, const float sr);
+float low_pass_filter_process (low_pass_filter *lp, float inp);
+void low_pass_filter_free(low_pass_filter *lpf);
+
 typedef struct {
    float fc;
    float sr;
@@ -22,15 +26,24 @@ typedef struct {
 }rms_filter;
 
 
-low_pass_filter *low_pass_filter_new(const float fc, const float sr);
-float low_pass_filter_process (low_pass_filter *lp, float inp);
-void low_pass_filter_free(low_pass_filter *lpf);
-
-
 rms_filter *rms_filter_new(const float fc, const float sr);
 float rms_filter_process (rms_filter *rms, const float inp);
-
-
 void rms_filter_free(rms_filter *rms);
+
+typedef struct {
+   float fcAttack;
+   float fcRelease;
+   float attackState;
+   float releaseState;
+   float sr;
+   low_pass_filter * attackFilter;
+   low_pass_filter * releaseFilter;
+}dynamics_filter;
+
+dynamics_filter *dynamics_filter_new(const float fc, const float attack_time, const float release_time);
+float dynamics_filter_process (dynamics_filter *dyn, const float inpRMS, const float threshold);
+void dynamics_filter_free(dynamics_filter *dyn);
+
+
 
 #endif
